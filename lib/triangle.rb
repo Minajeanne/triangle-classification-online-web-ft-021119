ed34c77cfd30e5856efb5df4cbc3b1ff48511db3
Alicia Santiago <albrown07@mac.com>
2019-03-04 18:10:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,9 +14,9 @@ class Triangle
       raise TriangleError
     elsif @side_1 + @side_2 == @side_1 + @side_3
         return :equilateral
-    elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3 || @side_2 == @side_3
+    elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
           return :isosceles
-    else #(@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+    else 
         return :scalene
     end
   end
