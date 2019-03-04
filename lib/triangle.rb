975782a010efe27ff44a709bbd72fd0dc4c856df
Alicia Santiago <albrown07@mac.com>
2019-03-04 17:44:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,14 +11,10 @@ class Triangle
     if (@side_1 <=0) || (@side_2 <= 0) || (@side_3 <= 0)
       begin
       raise TriangleError
-        rescue TriangleError => error
-          puts error.message
       end
     else (@side_2 + @side_3 <= @side_1) || (@side_3 + @side_1 <= @side_2) || (@side_2 + @side_1 <= @side_3)
       begin
       raise TriangleError
-        rescue TriangleError => error
-          puts error.message
       end
       else (@side_1 + @side_2) == (@side_2 + @side_3)
         return :equilateral
