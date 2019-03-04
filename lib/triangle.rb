0b28a305b9bf2294fd615ee5dd8a8766d825c4f4
Alicia Santiago <albrown07@mac.com>
2019-03-04 17:24:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,10 @@ class Triangle
   def kind(triangle)
     if (@side_1 + @side_2) == (@side_2 + @side_3)
       :equilateral
-      
+    elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+      :isosceles
+    elsif (@side_1 != side_2) && (side_2 != )
+
   end
 
   class TriangleError < StandardError
