82dca3c2e14fa24cb46987e2dfabd8545bd9f733
Alicia Santiago <albrown07@mac.com>
2019-03-04 17:17:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,13 +2,11 @@ class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
 
   def initialize(equilateral, isosceles, scalene)
-    @equilateral = equilateral
-    @isosceles = isosceles
-    @scalene = scalene
+    
   end
 
   def kind(triangle)
-    if self.triangle.size == 
+    if self.triangle.size ==
   end
 
   class TriangleError < StandardError
